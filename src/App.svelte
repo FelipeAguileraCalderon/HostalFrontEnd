6c5fc69cc4b1aa490cdb4a245e04650a1dd0911f
Felipe <felipe.aguilera@alumnos.ucn.cl>
2022-06-07 17:56:44 -0400

    build: Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Router, Route, navigate } from "svelte-navigator";
+  import Header from "./components/Header.svelte";
+</script>
+
+
+<Router>
+  <Route path="/">
+    {navigate("/home", { replace: true })}
+  </Route>
+  <Route path="home">
+    <Header></Header>
+  </Route>
+</Router>
+
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+    
+</script>
+
+<main>
+    <div>
+        <h1>Hola Mundo</h1>
+    </div>
+</main>
+
+<style>
+    h1{
+        color: black;
+    }
+</style>
