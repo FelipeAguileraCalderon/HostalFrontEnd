12880b882a1ee8c81d27e787fe8a5d6364e485db
Felipe <felipe.aguilera@alumnos.ucn.cl>
2022-06-07 18:59:28 -0400

    feat: Login HTML

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -1,9 +1,32 @@
 <script>
 
+    const error = ''
+
+    const handleSubmit = (e) => {}
+
+    const handleChange = (e) => {}
+
 </script>
 
 <main>
-    <h1>Estoy en Login</h1>
+    <div class='login-main-container'>
+        <form class='login-form' on:submit={handleSubmit}>
+            <div class='login-img-container'>
+                <img class='login-img' src='src\assets\HostalRemovedBG.png' alt='logo'/>
+            </div>
+            <div class='login-data-container'>
+                <div>
+                    <h3 class='login-h3'>Correo electrónico:</h3>
+                    <input type='email' name='email' on:change={handleChange} placeholder='youremail@company.com' class='login-input'/>
+                    <h3 class='login-h3'>Contraseña:</h3>
+                    <input type='password' on:change={handleChange} name='password' class='login-input'/>  
+                </div>
+                <div class='login-submit-container'>
+                    <input type='submit' value='Iniciar sesión'/>
+                </div>
+            </div>
+        </form>
+    </div>
 </main>
 
 <style>
